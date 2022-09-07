680a2a4096fa831ec3e67dfbc160491611392faa
dnblank123 <67945097+dnblank123@users.noreply.github.com>
2022-09-07 13:52:15 +0800

    dds

--- script.bat
+++ script.bat
@@ -13,6 +13,7 @@ echo 1 = Extract only *.LUA\*.XML
 echo 2 = Extract all client resources\mapdata w/o:avi,ogg,dds,mp3,wav
 echo 3 = Extract all client resource\mapdata
 echo 4 = Extract all ANI
+echo 5 = Extract all DDS
 SET/P mode="Enter extraction mode [default: %mode%]:"
 SET/P outputdir=Enter directory name [default: %outputdir%]:
 
@@ -21,6 +22,7 @@ if /i %mode%==1 goto only_script
 if /i %mode%==2 goto all_noaudiovideo
 if /i %mode%==3 goto all
 if /i %mode%==4 goto ani
+if /i %mode%==5 goto dds
 
 
 :only_dnt
@@ -47,6 +49,12 @@ goto finish
 FOR %%A IN (0 1 2 3 4 5 6 7 8 9) DO quickbms.exe -Y -f "*.ani" quickbms.txt Resource0%%A.pak %outputdir%
 FOR %%B IN (10 11 12 13 14 15 16 17 18 19) DO quickbms.exe -Y -f "*.ani" quickbms.txt Resource%%B.pak %outputdir%
 FOR %%C IN (20 21 22 23) DO quickbms.exe -Y -f "*.ani" quickbms.txt Resource%%C.pak %outputdir%
+goto finish
+
+:dds
+FOR %%A IN (0 1 2 3 4 5 6 7 8 9) DO quickbms.exe -Y -f "*.dds" quickbms.txt Resource0%%A.pak %outputdir%
+FOR %%B IN (10 11 12 13 14 15 16 17 18 19) DO quickbms.exe -Y -f "*.dds" quickbms.txt Resource%%B.pak %outputdir%
+FOR %%C IN (20 21 22 23) DO quickbms.exe -Y -f "*.dds" quickbms.txt Resource%%C.pak %outputdir%
 goto finish
 
 :finish
